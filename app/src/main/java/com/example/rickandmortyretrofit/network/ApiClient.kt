7ecec6b29e0ea6842c76656ed28628ee7e206107
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2022-05-03 23:43:55 -0500

    Created Character and ApiClient Kotlin files in network pkg

---
+++ app/src/main/java/com/example/rickandmortyretrofit/network/ApiClient.kt
@@ -0,0 +1,37 @@
+package com.example.rickandmortyretrofit.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+object ApiClient {
+
+    // BASE URL:
+    private val BASE_URL = "https://rickandmortyapi.com/api/"
+
+    // Variable for Moshi builder
+    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    // Create an instance of retrofit by lazy so it can initialize only when needed
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    val apiService: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+}
+
+// API Interface
+
+interface ApiService {
+    @GET("character")
+    fun fetchCharacters(@Query("page") page:String): Call<CharacterResponse>
+}
