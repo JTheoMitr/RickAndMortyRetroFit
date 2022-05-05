b5cb8f0d47e13ca7673be7b4f9ecd0c1f90eaef2
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2022-05-05 15:34:46 -0500

    Created Repository class and updated gradle (app) file dependencies

---
+++ app/src/main/java/com/example/rickandmortyretrofit/Repository.kt
@@ -0,0 +1,8 @@
+package com.example.rickandmortyretrofit
+
+import com.example.rickandmortyretrofit.network.ApiService
+
+class Repository(private val apiService: ApiService) {
+
+    fun getCharacters(page:String) = apiService.fetchCharacters(page)
+}
