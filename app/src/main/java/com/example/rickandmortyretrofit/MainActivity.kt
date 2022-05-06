e7c2a4910ae628a377fd6a159ef455e58b17fa20
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2022-05-06 14:50:36 -0500

    Updated Main Activity and added processCharacterResponse function to handle application state

--- app/src/main/java/com/example/rickandmortyretrofit/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortyretrofit/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.rickandmortyretrofit.network.Character
 import com.example.rickandmortyretrofit.network.ApiClient
 import com.example.rickandmortyretrofit.network.CharacterResponse
 import retrofit2.Call
@@ -21,14 +22,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel.characterLiveData.observe(this) { characters ->
-            val adapter = MainAdapter(characters)
-            val recyclerView = findViewById<RecyclerView>(R.id.charactersRv)
-
-            recyclerView?.layoutManager =
-                StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-            recyclerView?.adapter = adapter
+        viewModel.characterLiveData.observe(this) { state ->
+            processCharacterResponse(state)
         }
 
+
     }
+
+    private fun processCharacterResponse(state: ScreenState<List<Character>?>) {
+        when (state) {
+            is ScreenState.Loading -> {
+                // TODO Add Progress Bar
+            }
+            is ScreenState.Success -> {
+                if(state.data != null) {
+                    val adapter = MainAdapter(state.data)
+                    val recyclerView = findViewById<RecyclerView>(R.id.charactersRv)
+
+                    recyclerView?.layoutManager =
+                        StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+                    recyclerView?.adapter = adapter
+                }
+            }
+            is ScreenState.Error -> {
+
+            }
+        }
+    }
+
 }
